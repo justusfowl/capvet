aca8d1b07697a7588074cc77e9c53187a5c31b34
JustusFowl <justusfowl@gmail.com>
2019-09-21 14:30:04 +0200

    Creating a ShinyApp and very basic scenario / predictive modelling for suicide rates in dependency of medical and insurance expenditures

--- ddmr/CapVet/server.R
+++ ddmr/CapVet/server.R
@@ -1,25 +1,191 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(shinydashboard, leaflet, geojsonio, dplyr, shiny, shinyjs, readr, stringr)
 
-library(shiny)
+load("models.Rda")
+
+path_prefix <- ""
+
+df_lookup_city <- read_csv(paste0(path_prefix,"data/checked_lookup_va_med_city.csv"))
+df_sail_data <- read_csv(paste0(path_prefix,"data/SAIL.csv"))
+df_suicide_data <- read_csv(paste0(path_prefix,"data/state_suicide_data_2_agerange.csv"))
+
+df_suicide_data <- rename(df_suicide_data, 'State' = 'State of Death') 
+
+df_states_mapping <- read_csv(paste0(path_prefix,"data/states.csv"))
+
+va_exp_cols <- cols(
+  StateLong = col_character(),
+  State = col_character(),
+  `Veteran Population` = col_double(),
+  TotalExpenditures = col_double(),
+  CompensationAndPension = col_double(),
+  Construction = col_double(),
+  EducationAndVocRehab = col_character(),
+  LoanGuaranty = col_double(),
+  OperatingExp = col_double(),
+  InsuranceAndIndemnities = col_double(),
+  MedicalAndGOE = col_double(),
+  UniquePatient = col_double(),
+  Year = col_double()
+)
+
+df_va_exp <- read_delim(paste0(path_prefix,"data/va_expenditures_state_2005-2016.csv"), ";", col_types=va_exp_cols)
+
+# clean for individual states only
+df_suicide_data_rel  <- df_suicide_data[df_suicide_data$State != 'Total U.S.' & df_suicide_data$State != 'All' ,]
+
+# join state abbreviations
+df_suicide_base <- left_join(df_suicide_data_rel, df_states_mapping, by='State')
+df_suicide_base <- df_suicide_base %>% dplyr::select(-State) %>% rename(State = "Abbreviation")
+
+#rename columns
+
+df_suicide_base <- rename(df_suicide_base, VetSuicideRatePer100k=`Veteran Suicide Rate per 100,000`)
+df_suicide_base <- rename(df_suicide_base, GenPopSuicideRatePer100k=`General Population Rate per 100,000`)
+
+clean_rate <- function(x, col){
+  
+  val <- x[col]
+  
+  val <- str_replace_all(val, "[*]", "")
+  val <- str_replace_all(val, "-", "")
+  
+  val <- as.numeric(val)
+  
+}
+
+# clean suicide rates 
+df_suicide_base$VetSuicideRatePer100k <- apply(df_suicide_base, 1, function(x) clean_rate(x, col="VetSuicideRatePer100k"))
+df_suicide_base[is.na(df_suicide_base$VetSuicideRatePer100k),]$VetSuicideRatePer100k <- 0
+
+df_suicide_base$GenPopSuicideRatePer100k <- apply(df_suicide_base, 1, function(x) clean_rate(x, col="GenPopSuicideRatePer100k"))
+df_suicide_base[is.na(df_suicide_base$GenPopSuicideRatePer100k),]$GenPopSuicideRatePer100k = 0
+
+# focus on state-based rates overall only
+df_suicide_base <- df_suicide_base[df_suicide_base$`Age Group` == 'Total', ]
+
+# subset on the ones where suicide rate > 0 
+df_suicide_base <- df_suicide_base[df_suicide_base$VetSuicideRatePer100k > 0, ]
+
+df_states <- rename(df_states_mapping, StateLong = State)
+
+df_suicide_base <- dplyr::left_join(df_suicide_base, df_states, by=c("State" = "Abbreviation"))
+
+df_base_overtime <- left_join(df_suicide_base, df_va_exp, by=c("State"="State", "Year"="Year"))
+
+
+predict_vs_rate <- function(target_model, in_state, in_med, in_vet_pop, in_insurance, in_year){
+  
+  df_pred <- data.frame(matrix(ncol = 5, nrow = 1))
+  x <- c("State", "MedicalAndGOE", "VeteranPopulation", "InsuranceAndIndemnities", "Year")
+  colnames(df_pred) <- x
+  
+  df_pred$State <- factor(in_state,  levels = unique(df_states$Abbreviation))
+  df_pred$MedicalAndGOE <- in_med
+  df_pred$VeteranPopulation <- in_vet_pop
+  df_pred$InsuranceAndIndemnities <- in_insurance
+  df_pred$Year <- in_year
+  
+  if (target_model == "lm"){
+    res <- predict(lm_model, df_pred)
+    
+  }else if (target_model == "svm"){
+    res <- predict(svm_model, df_pred)
+  }else{
+    stop("Unknown model type")
+  }
+  
+  res
+  
+}
+
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+  
+  addClass(selector = "body", class = "sidebar-collapse")
+  
+  get_data <- reactive({
+    year <- input$displayYear
+    
+    # Requested State -> Long format
+    req_state <- input$stateSelect
+    
+    out <- df_suicide_base[df_suicide_base$Year == year,]
+    
+    if (req_state != "ALL"){
+      out <- out[out$StateLong == req_state,]
+    }
+    
+    out
+    
+  })
    
-  output$distPlot <- renderPlot({
+  output$stateMap <- renderLeaflet({
     
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    # create breaks from histogram
+    states <- geojsonio::geojson_read("us_states_geojson.geojson", what = "sp")
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    data <- get_data()
+    
+    if (input$stateSelect != 'ALL'){
+      states <- states[states$name == input$stateSelect,]
+    }
+    
+    pal <-  colorNumeric("Blues", df_suicide_base$VetSuicideRatePer100k, na.color = "#808080", alpha = TRUE, reverse = FALSE)
+
+    df_states_mapping_json <- rename(df_states_mapping, StateLong = State)
+    df_states_mapping_json <- rename(df_states_mapping_json, State = Abbreviation)
+    
+    # join geojson data lookup values of states
+    states@data <- dplyr::left_join(states@data, df_states_mapping_json, by=c("name" = "StateLong"))
+    states@data <- dplyr::left_join(states@data, data, by=c("State" = "State"))
+    
+    out <- leaflet(states) %>%
+      addTiles() %>%
+      addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 0.8,
+                  fillColor = ~pal((VetSuicideRatePer100k)),
+                  label = ~paste0(name, ": ", formatC(VetSuicideRatePer100k, big.mark = ",")))
+    
+    if (input$stateSelect == 'ALL'){
+      out <- out %>% addLegend("bottomright", pal = pal, values = ~VetSuicideRatePer100k,
+                  title = "Vet. Suicide Rates (per 100k)",
+                  labFormat = labelFormat(prefix = " "),
+                  opacity = 1
+        )
+    }
+    
+    out
+    
+    
+  })
+  
+  output$actualSuicideRate <- renderValueBox({
+    
+    data <- get_data()
+    valueBox(
+      paste0(round(mean(data$VetSuicideRatePer100k),2)), "AVG. VET SUICIDES (PER 100k)", icon = icon("user"),
+      color = "blue"
+    )
+  })
+  
+  output$predictedSuicideRate <- renderValueBox({
+    
+    target_model <- "lm"
+    
+    in_state <- df_states[df_states$StateLong == input$scenarioStateSelect,]$Abbreviation
+    
+    in_med <- input$scenarioMedExp
+    in_vet_pop <- input$scenarioVetPop
+    in_insurance <- input$scenarioInsurance
+    in_year <- 2016
+    
+    res <- predict_vs_rate(target_model, in_state, in_med = in_med, in_vet_pop = in_vet_pop, in_insurance = in_insurance, in_year = in_year)
+    
+    valueBox(
+      paste0(round(res,2)), "AVG. VET SUICIDES (PER 100k)", icon = icon("user"),
+      color = "blue"
+    )
     
   })
   
--- ddmr/CapVet/ui.R
+++ ddmr/CapVet/ui.R
@@ -1,33 +1,64 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(shinydashboard, plotly, leaflet, shinycssloaders, shinyjs, readr, rlang)
 
-library(shiny)
+path_prefix <- ""
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+df_states_mapping <- read_csv(paste0(path_prefix,"data/states.csv"))
+
+
+state_choices <- c("All" = "ALL")
+scenario_state_choices <- c()
+
+for (v in unique(df_states_mapping$State)){
+  state_choices <- c(state_choices, v)
+  scenario_state_choices <- c(scenario_state_choices, v)
+}
+
+state_choices <- state_choices %>% rlang::set_names(state_choices)
+scenario_state_choices <- scenario_state_choices %>% rlang::set_names(scenario_state_choices)
+
+dashboardPage(
+  dashboardHeader(title = "Capstone | Veteran Suicide Analysis"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard"))
+    )
+  ),
+  dashboardBody(
+    useShinyjs(),
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              
+              fluidRow(
+                box(width=12, h1("Actuals of Veteran Suicide Rates across the US"), p("The following chart illustrates the veteran suicide rates per different states in the US for a given year.")),
+                box( width = 3,
+                  selectInput("stateSelect", h4("STATE:"), choices = state_choices)
+                ),
+                box( width = 6,
+                     sliderInput("displayYear", h4("YEAR:"), min=2005, max=2016, value=2016, step=1)
+                ),
+                
+                infoBoxOutput("actualSuicideRate", width=3),
+                box(width=12, withSpinner(leafletOutput("stateMap")))
+              ),
+              fluidRow(
+                box(width=12, h1("Planning VA Expenditure in Dependency of Vet. Suicide Rates"), 
+                    p("The tool can be used for planning purposes and scenario modelling of VA expenditure and resulting suicide rates simulating effects for year 2016"), 
+                    selectInput("scenarioStateSelect", "", choices = scenario_state_choices)),
+                box( width = 3,
+                     sliderInput("scenarioMedExp", h4("Medical Expenditures:"), min=0, max=7000000, value=400000, step=1000)
+                ),
+                box( width = 3,
+                     sliderInput("scenarioVetPop", h4("Veteran Population:"), min=0, max=3000000, value=2000000, step=1000)
+                ),
+                box( width = 3,
+                     sliderInput("scenarioInsurance", h4("InsuranceAndIndemnities:"), min=0, max=200000, value=10000, step=1000)
+                ),
+                infoBoxOutput("predictedSuicideRate", width=3)
+              )
+              
+      )
     )
   )
-))
+)
